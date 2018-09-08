6578e531a8c66a24f55396354cd7d256535d084f
paul.bourdel <paul.bourdel@slalom.com>
2018-09-08 10:49:03 -0500

    Adding unauthorized status

--- src/main/java/com/bev/status/StatusController.java
+++ src/main/java/com/bev/status/StatusController.java
@@ -2,15 +2,22 @@ package com.bev.status;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatusController {
-    
+
     @GetMapping("/ifttt/v1/status")
     @ResponseBody
     public ResponseEntity status(@RequestHeader("IFTTT-Channel-Key") String iftttChannelKey) {
-        return new ResponseEntity(HttpStatus.FORBIDDEN);
+        if ("INVALID".equals(iftttChannelKey)) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        } else {
+            return new ResponseEntity(HttpStatus.OK);
+        }
     }
-    
+
 }
