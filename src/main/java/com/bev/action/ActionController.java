30cd168029bb9012ec223b82aafcd617d666d3b1
paul.bourdel <paul.bourdel@slalom.com>
2018-09-08 11:31:21 -0500

    new version

---
+++ src/main/java/com/bev/action/ActionController.java
@@ -0,0 +1,24 @@
+package com.bev.action;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ifttt/v1/actions")
+public class ActionController {
+
+    @GetMapping("start_car")
+    @ResponseBody
+    public ResponseEntity startCar(@RequestHeader("IFTTT-Channel-Key") String iftttChannelKey) {
+        if ("INVALID".equals(iftttChannelKey)) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        } else {
+            return new ResponseEntity(HttpStatus.OK);
+        }
+    }
+
+}
