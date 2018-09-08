53901833d1cce546f169db9fc399b4770b64cd0f
paul.bourdel <paul.bourdel@slalom.com>
2018-09-08 10:24:02 -0500

    adding web.config

--- terraform/output.tf
+++ terraform/output.tf
@@ -5,3 +5,8 @@ output "source_control" {
 output "site_credential" {
   value = "${azurerm_app_service.ifttt.site_credential}"
 }
+
+output "default_site_hostname" {
+  value = "${azurerm_app_service.ifttt.default_site_hostname}"
+}
+
