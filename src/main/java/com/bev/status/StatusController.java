b811c67557423dba91b8f5d4e92b677905a71e00
paul.bourdel <paul.bourdel@slalom.com>
2018-09-08 10:02:48 -0500

    Adding status controller

--- src/main/java/com/bev/status/StatusController.java
+++ src/main/java/com/bev/status/StatusController.java
@@ -1,14 +1,15 @@
 package com.bev.status;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class StatusController {
     
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
+    @GetMapping("/status")
+    public String status() {
+        return "OK";
     }
     
 }
