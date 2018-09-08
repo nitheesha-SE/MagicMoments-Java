e76b6ec3a12bd6f4c284475831f0eca53f47307a
paul.bourdel <paul.bourdel@slalom.com>
2018-09-08 10:57:00 -0500

    adding test setup

--- src/main/java/com/bev/status/StatusController.java
+++ src/main/java/com/bev/status/StatusController.java
@@ -2,10 +2,10 @@ package com.bev.status;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class StatusController {
@@ -17,6 +17,18 @@ public class StatusController {
             return new ResponseEntity(HttpStatus.UNAUTHORIZED);
         } else {
             return new ResponseEntity(HttpStatus.OK);
+        }
+    }
+
+    @PostMapping("/ifttt/v1/test/setup")
+    @ResponseBody
+    public ResponseEntity setup(@RequestHeader("IFTTT-Channel-Key") String iftttChannelKey) {
+        if ("INVALID".equals(iftttChannelKey)) {
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        } else {
+            Map body = new HashMap<String, Object>();
+            body.put("body", new HashMap<>());
+            return new ResponseEntity(body, HttpStatus.OK);
         }
     }
 
