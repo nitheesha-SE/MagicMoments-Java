fccaa881bd9b11db642c7c9c2b9363d7aa478a62
paul.bourdel <paul.bourdel@slalom.com>
2018-09-09 10:05:53 -0500

    new deploy

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
+    pollServer();
+});
+
+function pollServer(){
     $.ajax({
         url: "https://7d77d4069bab1b5b.azurewebsites.net/car"
         // url: "http://localhost:8080/car"
     }).then(function(data) {
-        $('.greeting-content').append(data.data[0].status);
-
-        if ("OFF" === data.data[0].status) {
-            $('#image-zoom img')
-                .attr("src", "https://production-assets2.autolist.com/production/lifestyle_gallery/hash/7/9/8/8bc3ad7cbdb948130c8ae062a4589ce740b0c897.jpg");
+        if ("ON" === data.data[0].status) {
+            $(".run").prop('checked',true);
         } else {
-            $('#image-zoom img')
-                .attr("src", "https://i.pinimg.com/236x/a6/e6/98/a6e6981a4c0cf678c3e18f02a4931695--ford-mondeo-ford-fusion.jpg");
-
+            $('.run').prop('checked',false);
         }
+        setTimeout(pollServer, 500);
     });
-});
+}
