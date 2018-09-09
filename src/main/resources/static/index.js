6e06965eedd8d9593c9878df59e2dab63915bc82
paul.bourdel <paul.bourdel@slalom.com>
2018-09-09 10:47:45 -0500

    new deploy

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
 
 function pollServer() {
     $.ajax({
-        // url: "https://7d77d4069bab1b5b.azurewebsites.net/car"
-        url: "http://localhost:8080/car"
+        url: "https://7d77d4069bab1b5b.azurewebsites.net/car"
+        // url: "http://localhost:8080/car"
     }).then(function (data) {
         var car = data.data[0].car;
 
