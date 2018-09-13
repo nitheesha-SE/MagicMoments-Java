ad2d6c74ee57a31653e3c63273862cfb355fda35
paul.bourdel <paul.bourdel@slalom.com>
2018-09-13 14:43:14 -0500

    new deploy

--- src/main/java/com/bev/Application.java
+++ src/main/java/com/bev/Application.java
@@ -1,20 +1,13 @@
 package com.bev;
 
-import com.microsoft.applicationinsights.TelemetryConfiguration;
-import com.microsoft.applicationinsights.web.internal.WebRequestTrackingFilter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.Filter;
 import java.util.Arrays;
 
 @SpringBootApplication
@@ -22,7 +15,6 @@ public class Application {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        // Do any additional configuration here
         return builder.build();
     }
 
@@ -43,37 +35,6 @@ public class Application {
             }
 
         };
-    }
-
-    @Configuration
-    public class AppInsightsConfig {
-
-        //Initialize AI TelemetryConfiguration via Spring Beans
-        @Bean
-        public String telemetryConfig() {
-            String telemetryKey = "5ffa55ca-1a86-42e1-b35c-2a54581f2d2e";
-            if (telemetryKey != null) {
-                TelemetryConfiguration.getActive().setInstrumentationKey(telemetryKey);
-            }
-            return telemetryKey;
-        }
-
-        //Set AI Web Request Tracking Filter
-        @Bean
-        public FilterRegistrationBean aiFilterRegistration(@Value("${spring.application.name:application}") String applicationName) {
-            FilterRegistrationBean registration = new FilterRegistrationBean();
-            registration.setFilter(new WebRequestTrackingFilter(applicationName));
-            registration.setName("webRequestTrackingFilter");
-            registration.addUrlPatterns("/*");
-            registration.setOrder(Ordered.HIGHEST_PRECEDENCE + 10);
-            return registration;
-        }
-
-        //Set up AI Web Request Tracking Filter
-        @Bean(name = "WebRequestTrackingFilter")
-        public Filter webRequestTrackingFilter(@Value("${spring.application.name:application}") String applicationName) {
-            return new WebRequestTrackingFilter(applicationName);
-        }
     }
 
 }
