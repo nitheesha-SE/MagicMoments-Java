e790cb04a51ce4bb7b456087196fffd50adf1cf8
paul.bourdel <paul.bourdel@slalom.com>
2018-09-14 15:40:08 -0500

    new deploy

--- src/main/java/com/bev/trigger/model/BatteryLevelEvent.java
+++ src/main/java/com/bev/trigger/model/BatteryLevelEvent.java
@@ -4,18 +4,18 @@ import java.time.Instant;
 
 public class BatteryLevelEvent extends TriggerEvent {
 
-    private Integer batteryLevel;
+    private Integer battery_level;
 
     public BatteryLevelEvent(String id, Instant createdAt, Integer batteryLevel) {
         super(id, createdAt);
-        this.batteryLevel = batteryLevel;
+        this.battery_level = batteryLevel;
     }
 
-    public Integer getBatteryLevel() {
-        return batteryLevel;
+    public Integer getBattery_level() {
+        return battery_level;
     }
 
-    public void setBatteryLevel(Integer batteryLevel) {
-        this.batteryLevel = batteryLevel;
+    public void setBattery_level(Integer battery_level) {
+        this.battery_level = battery_level;
     }
 }
