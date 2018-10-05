d696ad28aaa2ce0d622787655ff614347eae75d7
paul.bourdel <paul.bourdel@slalom.com>
2018-10-05 15:37:28 -0500

    new deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@ mvn clean package -DskipTests
 cp target/gs-spring-boot-0.1.0.jar .
 git add -A
 git commit -m "new deploy"
-git push
+git push origin
 curl -H "IFTTT-Channel-Key: VALID" https://7d77d4069bab1b5b.azurewebsites.net/ifttt/v1/status
